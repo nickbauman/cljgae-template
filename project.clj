c46a6c5741d0683515cbc594817ace1fc79a7e18
nickbauman <nick.bauman@cirrusmachina.com>
2016-11-29 22:08:07 -0600

    0.2.1 release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject org.clojars.nicolaus/cljgae-template "0.2.0-SNAPSHOT"
-  :description "CljGAE is a leiningen template for Google App Engine apps"
+(defproject org.clojars.nicolaus/cljgae-template "0.2.1-SNAPSHOT"
+  :description "cljgae-template is a leiningen template for Google App Engine apps"
   :url "https://github.com/nickbauman/cljgae-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
