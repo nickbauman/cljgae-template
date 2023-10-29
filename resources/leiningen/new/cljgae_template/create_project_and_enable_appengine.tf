9fab4ab97cae67b7a29812cca8ecae866d0f3203
nickbauman <nick.bauman@cirrusmachina.com>
2023-10-29 14:31:19 -0500

    turn on appropriate apis when creating a cloud project for appengine

--- resources/leiningen/new/cljgae_template/create_project_and_enable_appengine.tf
+++ resources/leiningen/new/cljgae_template/create_project_and_enable_appengine.tf
@@ -1,10 +1,20 @@
-# do not commit to source code control
+# QUICKSTART ONLY; do not commit to source code control
 resource "google_project" "{{name}}" {
   name       = "{{name}}"
   project_id = "{{name}}"
   org_id     = "{{org-id}}"
   billing_account = "{{billing-account}}"
   auto_create_network = true
+}
+
+locals {
+  enable_services = ["iam.googleapis.com","cloudscheduler.googleapis.com","datastore.googleapis.com","cloudresourcemanager.googleapis.com", "cloudtasks.googleapis.com", "cloudidentity.googleapis.com"]
+}
+
+resource "google_project_service" "{{name}}" {
+  project = "{{name}}"
+  for_each = toset(local.enable_services)
+  service = each.key
 }
 
 resource "google_app_engine_application" "app" {
