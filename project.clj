a9f11fe47573cb672568e10f2af6b24791a804b9
Nick Bauman <nick.bauman@gmail.com>
2024-07-01 18:20:10 -0500

    Tweak typo (#35)
    fix typo, bump project version

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject org.clojars.nickbauman/cljgae-template "1.0.3"
+(defproject org.clojars.nickbauman/cljgae-template "1.0.3.1"
   :description "cljgae-template is a leiningen template for Google App Engine apps Java11/17 Runtimes"
   :url "https://github.com/nickbauman/cljgae-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
-
